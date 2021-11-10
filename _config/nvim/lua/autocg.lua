2e5ce44e06aea071282029a1e9374a597dd4c664
beyondmagic <koetemagie@gmail.com>
2021-11-10 16:08:40 -0300

    nvim: added reload theme remote

--- _config/nvim/lua/autocg.lua
+++ _config/nvim/lua/autocg.lua
@@ -3,16 +3,17 @@
 --   Set spell check in markdown files
 --   When save xbindkeys, reset
 --   When save dunst, notify
---   Create commands that often I do mistakenly type
+--   Save session to nvim
+--   Signals (USR1toto reload theme
 vim.api.nvim_exec(
 [[
 
 autocmd! BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
 autocmd! FileType markdown setlocal spell!
-autocmd BufRead,BufNewFile COMMIT_EDITMSG setlocal spell!
+autocmd! BufRead,BufNewFile COMMIT_EDITMSG setlocal spell!
 autocmd! BufWritePost $HOME/Base/xbindkeysrc !eval "killall xbindkeys; xbindkeys -f /home/iris/Base/xbindkeysrc"
 autocmd! BufWritePost $HOME/.config/dunst/dunstrc !eval "killall dunst; dunst & notify-send -i /usr/share/icons/Adwaita/16x16/actions/folder-new-symbolic.symbolic.png 'Testing Dunst'"
 autocmd! VimLeave * :mksession! ~/.config/nvim/session.vim
-set conceallevel=0
+autocmd! Signal SIGUSR1 let &bg=(&bg=='light'?'dark':'light')
 
 ]], false)

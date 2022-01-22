3d5e3c223936d078457cd879ab01b43ab61d7d27
J.Farias (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2022-01-22 20:03:03 -0300

    EWW: fix TODO for correct handling of mpd states

--- .config/eww/bars/top/song_daemon.sh
+++ .config/eww/bars/top/song_daemon.sh
@@ -13,33 +13,57 @@
 # Possible values are "playing" and "paused".
 mpd_state () { mpc status '%state%'; }
 
+# To verify when the player comes back playing.
+last_state="$(mpd_state)"
+
+# To verify when a song changes.
+last_name="$(mpc current)"
+
 # Initial setup, will run once, it will get the current state of MPD and update to the correct value.
-[ "$(mpd_state)" = 'playing' ] && eww update reveal-songnames=true
+[ "$last_state" = 'playing' ] && eww update reveal-songnames=true
 
 # Loop MPD state changes, it gives two outputs, one of the server itself, another from the player.
-# TODO: Do not update on seeking MPC.
-mpc idleloop | while IFS=$'\n' read -r type_mpd && read -r type_mixer; do
+while : ; do
 
-  # Will update only on server changers.
-  [ $type_mpd = 'player' ] && {
+  mpc idle
 
-    # MPD is paused.
-    [ "$(mpd_state)" = 'paused' ] && {
+  # MPD is paused.
+  [ "$(mpd_state)" = 'paused' ] && {
 
-      # Disable EWW running the other script.
-      eww update reveal-songnames=false
+    # Disable EWW running the other script.
+    eww update reveal-songnames=false
 
-    # MPD is playing.
-    } || {
+    # To verify later if the song was paused.
+    last_state='paused'
+
+  # MPD is playing.
+  } || {
+
+    new_song="$(mpc current)"
+
+    # Song changed.
+    [ "$last_name" != "$new_song" ] && {
+
+      last_name="$new_song"
 
       # https://github.com/BeyondMagic/creamberry/blob/master/path/mpd_song
-      mpd_song
-
-      # Quick update of value and allow EWW to run the other script.
-      eww update reveal-songnames=false
-      eww update reveal-songnames=true
+      mpd_song &
 
     }
+
+    # If the last state was not playing.
+    [ "$last_state" = "paused" ] && {
+
+      # https://github.com/BeyondMagic/creamberry/blob/master/path/mpd_song
+      mpd_song &
+
+      last_state='playing'
+
+    }
+
+    # Quick update of value and allow EWW to run the other script.
+    eww update reveal-songnames=false
+    eww update reveal-songnames=true
 
   }
 

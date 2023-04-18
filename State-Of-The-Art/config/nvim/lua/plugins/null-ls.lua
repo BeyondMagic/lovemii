6158ec7433d8c38a1de1d5dc8aab3a5bb094fe33
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-04-18 09:27:23 -0300

    nvim: disable null-ls code-actions of gitsigns due to conflict
    with ccls with a different enconding (which ccls supports only utf-32)

--- State-Of-The-Art/config/nvim/lua/plugins/null-ls.lua
+++ State-Of-The-Art/config/nvim/lua/plugins/null-ls.lua
@@ -6,10 +6,11 @@ return {
 
     -- Register any number of sources simultaneously
     null_ls.setup {
+      filetypes = { "markdown", "text" },
       sources = {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.write_good,
-        null_ls.builtins.code_actions.gitsigns,
+        --null_ls.builtins.code_actions.gitsigns,
 
         -- Parse English
         --null_ls.builtins.diagnostics.vale.with {

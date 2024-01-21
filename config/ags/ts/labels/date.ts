3b3a9ebb6df955d9f9cb055737a57642a6a221e7
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-21 15:41:30 -0300

    label: add number of day and weekday date

--- config/ags/ts/labels/date.ts
+++ config/ags/ts/labels/date.ts
@@ -4,7 +4,7 @@ import Variable from 'resource:///com/github/Aylur/ags/variable.js'
  * Will retrieve the current date of the system in a interval.
  */
 const date = Variable('', {
-	poll: [1000, "date '+%B %I時%M分%p'"],
+	poll: [1000, "date '+%B %e%A %I時%M分%p'"],
 });
 
 export default date;

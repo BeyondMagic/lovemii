7a89af78b3da0ca0e4c111b98708bdcc9fc82bd2
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-22 13:39:54 -0300

    ags: add small format to day, weekday and month

--- config/ags/ts/variables/date.ts
+++ config/ags/ts/variables/date.ts
@@ -4,7 +4,12 @@ import Variable from 'resource:///com/github/Aylur/ags/variable.js'
  * Will retrieve the current date of the system in a interval.
  */
 const date = Variable('', {
-	poll: [1000, "date '+%B%e%A %I時%M分%p'", (out : string) => out.replace(/([一-龯]+)/g, '<span fgalpha="75%">$1</span>')],
+	poll: [1000,
+		"date '+%B%e%A %I時%M分%p'",
+		(out : string) =>
+			out
+			.replace(/([^\s]+)/, '<span size="smaller" rise="1pt">$1</span>')
+			.replace(/([一-龯]+)/g, '<span fgalpha="75%">$1</span>')],
 });
 
 export default date;

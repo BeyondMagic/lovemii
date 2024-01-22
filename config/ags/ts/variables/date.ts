9984b4de705b3324ca79d7999ad580cbae967901
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-21 22:05:05 -0300

    ags: folder labels to variables and add pango markup to date

--- config/ags/ts/variables/date.ts
+++ config/ags/ts/variables/date.ts
@@ -4,7 +4,7 @@ import Variable from 'resource:///com/github/Aylur/ags/variable.js'
  * Will retrieve the current date of the system in a interval.
  */
 const date = Variable('', {
-	poll: [1000, "date '+%B %e%A %I時%M分%p'"],
+	poll: [1000, "date '+%B%e%A %I時%M分%p'", (out : string) => out.replace(/([一-龯]+)/g, '<span fgalpha="75%">$1</span>')],
 });
 
 export default date;

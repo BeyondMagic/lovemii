a996ce848661c8c84fdf9b5527c5ad15809276d4
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-01-23 15:50:02 -0300

    ags.date: contain into an eventbox for future events

--- config/ags/ts/labels/date.ts
+++ config/ags/ts/labels/date.ts
@@ -1,14 +1,22 @@
-import { Label } from 'resource:///com/github/Aylur/ags/widget.js';
+import { EventBox, Label } from 'resource:///com/github/Aylur/ags/widget.js';
 import date from '../variables/date';
+import Gdk from 'node_modules/@girs/gdk-3.0/gdk-3.0';
 
 /* Label  */
-const LabelDate = (() => {
-	const widget = Label({ use_markup: true, class_name: 'label-date' });
+const LabelDate = EventBox({
+	class_name: 'eventbox-date',
+	child: (() => {
+		const widget = Label({ use_markup: true, class_name: 'label-date' });
 
-	// Change label (with Pango support) opon change of date.
-	widget.hook(date, (self) => self.set_label(date.value), 'notify::value')
+		// Change label (with Pango support) opon change of date.
+		widget.hook(date, (self) => self.set_label(date.value), 'notify::value')
 
-	return widget;
-})();
+		return widget;
+	})(),
+});
+
+LabelDate.connect('button-press-event', (_, object: Gdk.Event) => {
+	print("lol")
+});
 
 export default LabelDate;

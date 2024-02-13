df31900470b13a1a37f06aa2e5d6e47d36d64e7d
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-02-13 10:10:55 -0300

    ags: código mais simples

--- configuração/local/ags/ts/main.ts
+++ configuração/local/ags/ts/main.ts
@@ -1,18 +1,20 @@
-import SCSS, { css } from './subprocesses/scss';
+import SCSS, { css as style } from './subprocesses/scss';
 import Bar from './widgets/bar';
 import Corner from './widgets/screencorners';
 
 SCSS.reload();
 SCSS.launch_monitor();
 
+const windows = [
+	Corner('top', 'left'),
+	Corner('top', 'right'),
+	Corner('bottom', 'left'),
+	Corner('bottom', 'right'),
+	Bar(0),
+	// Calendar(),
+]
+
 export default {
-	windows: [
-		Corner('top', 'left'),
-		Corner('top', 'right'),
-		Corner('bottom', 'left'),
-		Corner('bottom', 'right'),
-		Bar(0),
-		// Calendar(),
-	],
-	style: css
+	windows,
+	style
 }

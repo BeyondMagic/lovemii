886ccfc8f3fdebfa8d9ab69a958035f2ca78c4a0
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-02-21 22:44:25 -0300

    ags: draw battery widget using cairo :)

--- configuração/local/ags/ts/widgets/battery.ts
+++ configuração/local/ags/ts/widgets/battery.ts
@@ -1,8 +1,53 @@
-import { Label, Box } from 'resource:///com/github/Aylur/ags/widget.js';
+import { Label } from 'resource:///com/github/Aylur/ags/widget.js';
 import Battery from 'resource:///com/github/Aylur/ags/service/battery.js'
 
-// Battery widget for the bar.
-const markup_percentage = '<span fgalpha="75%">%</span>'
+function draw_icon (label : ReturnType<typeof Label>, context : any) : void {
+
+	const label_height = label.get_allocated_height()
+	const label_width = label.get_allocated_width()
+
+	const circle_size = 3
+
+	const width = label_width - circle_size * circle_size
+	const height = width / 2.25
+
+	const x = (label_width - width) / 2
+	const y = (label_height - height) / 2
+
+	const radius = 6
+	const degrees = Math.PI / 180
+
+	const background = {
+		red: 1 - Battery.percent / 100,
+		green: Battery.percent / 100,
+		blue: 0,
+	}
+
+	context.newSubPath()
+	context.arc(x + width - radius, y + radius, radius, -90 * degrees, 0 * degrees)
+	context.arc(x + width - radius, y + height - radius, radius, 0 * degrees, 90 * degrees)
+	context.arc(x + radius, y + height - radius, radius, 90 * degrees, 180 * degrees)
+	context.arc(x + radius, y + radius, radius, 180 * degrees, 270 * degrees)
+	context.closePath()
+
+	context.setSourceRGBA(background.red, background.green, background.blue, 0.15)
+	context.fillPreserve()
+	context.setSourceRGBA(background.red, background.green, background.blue, 0.5)
+	context.setLineWidth(2)
+	context.stroke()
+
+	// Draw circle.
+	context.save()
+	context.arc(width + x + circle_size / 2, label_height / 2, circle_size, Math.PI * 3 / 2, Math.PI / 2)
+	context.setSourceRGBA(background.red, background.green, background.blue, 0.5)
+	context.fillPreserve()
+	context.restore()
+	context.stroke()
+
+
+	// Freeing the context before returning from the callback
+	context.$dispose()
+}
 
 function to_hex (c : number) : string {
 	const hex = Math.floor(c).toString(16)
@@ -10,12 +55,15 @@ function to_hex (c : number) : string {
 	return hex.length === 1 ? '0' + hex : hex
 }
 
+// Battery widget for the bar.
+const markup_percentage = '<span fgalpha="75%">%</span>'
+
 function colour (percentage : number) : string {
 	const red = (1 - percentage / 100) * 255
 	const green = percentage / 100 * 255
 	const blue = 0
 
-	return '<span foreground="#'
+	const number = '<span foreground="#'
 		+ to_hex(red)
 		+ to_hex(green)
 		+ to_hex(blue)
@@ -23,31 +71,25 @@ function colour (percentage : number) : string {
 		+ percentage
 		+ markup_percentage
 		+ '</span>'
+
+	return number
 }
 
-function charge (charging : boolean) : string {
-	return charging ? '⚡' : ''
-}
-
-const widget_number =  Box({
-	class_name: 'battery',
-	children: [
-		Label({
-			hexpand: true,
-			class_name: 'number',
-			use_markup: true,
-			label: Battery
-				.bind('percent')
-				.transform(p => colour(p)),
-		}),
-		Label({
-			class_name: 'charging',
-			use_markup: true,
-			label: Battery
-				.bind('charging')
-				.transform(charging => charge(charging))
+const widget_number = Label({
+	visible: Battery.bind('available'),
+	class_name: Battery.
+		bind('charging')
+		.transform(charging => {
+			return charging ? 'charging battery' : 'battery'
 		})
-	]
+	,
+	use_markup: true,
+	label: Battery
+		.bind('percent')
+		.transform(percentage => colour(percentage)),
+	
+	setup: self => self
+		.connect('draw', (_, context) => draw_icon(self, context))
 })
 
 const battery = {

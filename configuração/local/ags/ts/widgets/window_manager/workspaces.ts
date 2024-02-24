9a190065a145af06546c20d505c0ae461cb7fe03
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-02-24 00:03:38 -0300

    ags: special workspaces are now set dynamically :)

--- configuração/local/ags/ts/widgets/window_manager/workspaces.ts
+++ configuração/local/ags/ts/widgets/window_manager/workspaces.ts
@@ -17,28 +17,38 @@ function create_button (id : number, name : string) {
 		hexpand: false,
 
 		setup (self) {
+
+			let class_name = 'symbol'
+
+			// Special workspaces.
 			if (id < 0)
 			{
-				const [ title, workspace ] = name.split(' ')
-
 				self.on_clicked = () => set_special_workspace(workspace)
 
-				self.child = Icon({
-					hpack: 'center',
-					class_name: 'symbol icon',
-					icon: title + '-symbolic',
-					size: data.settings.icon_size,
-				})
+				const [ title, workspace ] = name.split(' ')
 
-			} else {
+				// If it was assigned dynamically (not upon data.toml)
+				// we should use a label instead.
+				if (workspace)
+					return self.child = Icon({
+						hpack: 'center',
+						class_name: class_name + ' icon ' + title,
+						icon: title + '-symbolic',
+						size: data.settings.icon_size,
+					})
+
+				// Remove the 'special:' substring of the special workspace's name.
+				name = name.substring(name.indexOf(':') + 1)
+				class_name += ' ' + name
+			} else
+				// Normal workspaces.
 				self.on_clicked = () => set_workspace(id)
 
-				self.child = Label({
-					class_name: 'symbol',
-					hexpand: false,
-					label: name,
-				})
-			}
+			self.child = Label({
+				class_name,
+				hexpand: false,
+				label: name,
+			})
 		}
 	})
 }
@@ -104,14 +114,27 @@ function workspace_switch () : void {
 }
 
 function remove_workspace (id : number | undefined) {
-	if (id === undefined)
-		return
-
+	// Some special workspace was removed.
 	if (id === 0)
 	{
-		workspace_switch()
-		return
+		// Since we don't have its id, let's map all ids of the buttons
+		// to the Hyprland workspaces that no longer appear.
+		const special_workspaces = new Map<number, boolean>()
+
+		for (const workspace of Hyprland.workspaces)
+			if (workspace.id < 0)
+				special_workspaces.set(id, true)
+
+		for (const [ button_id ] of buttons)
+			if (button_id < 0 && !special_workspaces.has(button_id))
+				remove_workspace(button_id)
+
+		// See if they are active or not.
+		return workspace_switch()
 	}
+
+	if (id === undefined)
+		return
 
 	if (permanent_buttons.has(id))
 		return
@@ -121,28 +144,39 @@ function remove_workspace (id : number | undefined) {
 	buttons.delete(id)
 }
 
-
 function add_workspace (box : ReturnType<typeof Box>, id : number | undefined) {
 	if (id === undefined)
 		return
 
+	let workspace : typeof Hyprland.workspaces[0] | undefined
+
+	// Some special workspace was added.
 	if (id === 0)
 	{
-		workspace_switch()
-		return
+		// Since we don't have its id, let's try to add all of them.
+		for (const w of Hyprland.workspaces)
+			if (w.id < 0)
+				add_workspace(box, w.id)
+
+		// See if they are active or not.
+		return workspace_switch()
 	}
 
-	if (buttons.has(id))
+
+	// The normal workspace already exists.
+	else if (buttons.has(id))
 		return
 
-	for (const workspace of Hyprland.workspaces)
-		if (workspace.id === id)
-		{
-			const button = create_button(id, workspace.name)
-			buttons.set(id, button)
-			box.children = box.children.concat(button)
-			return
-		}
+	// The normal workspace does not exist, so we get its content.
+	else
+		workspace = Hyprland.workspaces.find(workspace => workspace.id === id)
+
+	if (workspace)
+	{
+		const button = create_button(workspace.id, workspace.name)
+		buttons.set(workspace.id, button)
+		box.children = box.children.concat(button)
+	}
 }
 
 function event_handler (box : ReturnType<typeof Box>, event : string, data : string) {

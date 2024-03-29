32db680377df363faf84519c99724ed3dd378485
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-03-29 09:06:23 -0300

    nvim: update zig language server settings

--- configuração/local/nvim/lua/lsp/zig.lua
+++ configuração/local/nvim/lua/lsp/zig.lua
@@ -1,1 +1,11 @@
-require'lspconfig'.zls.setup{}
+require 'lspconfig'.zls.setup {
+	capabilities = CAPABILITIES,
+
+	on_attach = function(client, bufnr)
+
+		if client.server_capabilities.documentSymbolProvider then
+			require("nvim-navic").attach(client, bufnr)
+		end
+
+	end
+}

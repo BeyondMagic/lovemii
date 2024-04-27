30294fe9007e3a699e4af6a64c113afa3bdd80a7
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-04-27 12:35:04 -0300

    ags: update assets and

--- configuração/local/ags/ts/main.ts
+++ configuração/local/ags/ts/main.ts
@@ -2,8 +2,8 @@ import SCSS, { css as style } from './subprocesses/scss'
 import windows from './windows';
 import type { Config } from '../types/app.d.ts';
 
-SCSS.reload();
-SCSS.launch_monitor();
+SCSS.reload()
+SCSS.launch_monitor()
 
 const configuration : Config = {
 	style,

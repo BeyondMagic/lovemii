cda7f066b725b17a9c1acf1f5b7f3afa8170d7bf
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-11-28 23:17:58 -0300

    fix(nvim): update git signs config

--- configuração/local/nvim/lua/plugins/git-signs.lua
+++ configuração/local/nvim/lua/plugins/git-signs.lua
@@ -2,13 +2,6 @@
 return { 'lewis6991/gitsigns.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = {
-    signs = {
-      add          = {hl = 'GitSignsAdd'   , text = '┃', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-      change       = {hl = 'GitSignsChange', text = '┃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-      delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    },
     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
     numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
     linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -40,14 +33,11 @@ return { 'lewis6991/gitsigns.nvim',
       follow_files = true
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
       delay = 1000,
-    },
-    current_line_blame_formatter_opts = {
-      relative_time = false
     },
     sign_priority = 6,
     update_debounce = 100,
@@ -60,9 +50,6 @@ return { 'lewis6991/gitsigns.nvim',
       relative = 'cursor',
       row = 0,
       col = 1
-    },
-    yadm = {
-      enable = false
     },
   }
 }

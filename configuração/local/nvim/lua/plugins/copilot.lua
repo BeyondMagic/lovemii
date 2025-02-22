26b37389fd325a5eb673ad69c1746afab6ad9c55
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-02-22 18:31:37 -0300

    feat(nvim): add copilot plugin

---
+++ configuração/local/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,62 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = false,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-CR>"
+				},
+				layout = {
+					position = "bottom", -- | top | left | right | horizontal | vertical
+					ratio = 0.4
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = false,
+				hide_during_completion = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-k>",
+					prev = "<C-j>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				["*"] = false,
+				["."] = false,
+				yaml = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+
+				lua = true,
+				markdown = true,
+				javascript = true,
+				typescript = true,
+				rust = true,
+				c = true,
+				["c#"] = true,
+				["c++"] = true,
+				go = true,
+				python = true,
+			},
+			copilot_node_command = 'node', -- Node.js version must be > 18.x
+			server_opts_overrides = {},
+		})
+	end,
+}

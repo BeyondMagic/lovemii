a6ca85fe1c4d0bb78fa91eda71da407df428e258
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-03-08 05:58:16 -0300

    feat(ags): add corner unified function and simplify bar

--- configuração/local/ags/app.ts
+++ configuração/local/ags/app.ts
@@ -1,10 +1,42 @@
-import { App } from "astal/gtk4"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import { App, Gtk, Astal } from "astal/gtk4";
+import Hyprland from "gi://AstalHyprland";
+
+import style from "./scss/style.scss";
+
+import Corner from "./widget/components/Corner";
+import Bar from "./widget/Bar";
 
 App.start({
-    css: style,
-    main() {
-        App.get_monitors().map(Bar)
-    },
-})
+  icons: `${SRC}/assets`,
+  css: style,
+  main() {
+    const hyprland = Hyprland.get_default();
+    hyprland.get_monitors().map((monitor) => {
+      Bar(monitor.id);
+      Corner('top-left', monitor.id);
+      Corner('top-right', monitor.id);
+      Corner('bottom-left', monitor.id);
+      Corner('bottom-right', monitor.id);
+    });
+  },
+  requestHandler(request: string, res: (response: any) => void) {
+    // switch (request) {
+    //   case "applauncher":
+    //     App.toggle_window("Applauncher");
+    //     break;
+    //   case "dashboard":
+    //     App.toggle_window("Dashboard");
+    //     break;
+    //   case "pldashboard":
+    //     App.toggle_window("PlayerDashboard");
+    //     break;
+    //   case "poweractions":
+    //     App.toggle_window("PowerActions");
+    //     break;
+    //   case "clearnotification":
+    //     clearLastNotification();
+    //     break;
+    // }
+    // return res("toggled window");
+  },
+});

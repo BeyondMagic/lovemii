57b557291891bff3d1848e39baf127b08a40234c
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-05-26 16:17:57 -0300

    feat(nvim.plugins): replace null-ls with hl-chunk.nvim

---
+++ configuração/local/nvim/lua/plugins/hlchunk-nvim.lua
@@ -0,0 +1,22 @@
+return {
+	"shellRaining/hlchunk.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("hlchunk").setup({
+			chunk = {
+				enable = true
+				-- ...
+			},
+			indent = {
+				enable = true
+				-- ...
+			},
+			line_num = {
+				enable = true
+			},
+			blank = {
+				enable = true
+			}
+		})
+	end
+}

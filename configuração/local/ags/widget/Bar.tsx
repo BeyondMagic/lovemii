66cd1ba2c5fa3a330108366fcdc83a04356ad66b
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-07-17 18:05:49 -0300

    style(ags): 2 spaces to 4-width tabs

--- configuração/local/ags/widget/Bar.tsx
+++ configuração/local/ags/widget/Bar.tsx
@@ -4,36 +4,36 @@ import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-  const time = createPoll("", 1000, "date")
-  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+	const time = createPoll("", 1000, "date")
+	const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-  return (
-    <window
-      visible
-      name="bar"
-      class="Bar"
-      gdkmonitor={gdkmonitor}
-      exclusivity={Astal.Exclusivity.EXCLUSIVE}
-      anchor={TOP | LEFT | RIGHT}
-      application={app}
-    >
-      <centerbox cssName="centerbox">
-        <button
-          $type="start"
-          onClicked={() => execAsync("echo hello").then(console.log)}
-          hexpand
-          halign={Gtk.Align.CENTER}
-        >
-          <label label="Welcome to AGS!" />
-        </button>
-        <box $type="center" />
-        <menubutton $type="end" hexpand halign={Gtk.Align.CENTER}>
-          <label label={time} />
-          <popover>
-            <Gtk.Calendar />
-          </popover>
-        </menubutton>
-      </centerbox>
-    </window>
-  )
+	return (
+		<window
+			visible
+			name="bar"
+			class="Bar"
+			gdkmonitor={gdkmonitor}
+			exclusivity={Astal.Exclusivity.EXCLUSIVE}
+			anchor={TOP | LEFT | RIGHT}
+			application={app}
+		>
+			<centerbox cssName="centerbox">
+				<button
+					$type="start"
+					onClicked={() => execAsync("echo hello").then(console.log)}
+					hexpand
+					halign={Gtk.Align.CENTER}
+				>
+					<label label="wow!" />
+				</button>
+				<box $type="center" />
+				<menubutton $type="end" hexpand halign={Gtk.Align.CENTER}>
+					<label label={time} />
+					<popover>
+						<Gtk.Calendar />
+					</popover>
+				</menubutton>
+			</centerbox>
+		</window>
+	)
 }

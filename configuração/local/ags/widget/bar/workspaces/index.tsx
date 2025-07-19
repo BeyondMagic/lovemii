0d269f1e3fc3adff58263f171498edb927b4bfab
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-07-19 03:03:17 -0300

    feat(ags): add workspaces to bar

--- configuração/local/ags/widget/bar/workspaces/index.tsx
+++ configuração/local/ags/widget/bar/workspaces/index.tsx
@@ -1,7 +1,55 @@
-import { execAsync } from "ags/process"
 import { Astal, Gtk, Gdk } from "ags/gtk4"
+import { hyprland } from "../../../services/hyprland";
+import { createBinding, createComputed, For } from "ags"
+import { config } from "../../../app";
+import Hyprland from "gi://AstalHyprland";
 
-export function Workspaces ()
+const focused = createBinding(hyprland, "focused_workspace")
+const workspaces = createBinding(hyprland, "workspaces")
+
+function Workspace ({ ws } : { ws: Hyprland.Workspace })
 {
-	return <box/>;
+	const clients = createBinding(ws, "clients")
+	const name = createBinding(ws, "name").as((n) => n ?? `${ws.id}`)
+
+	const classes = createComputed([clients, focused], (cs, f) => [
+		"workspace",
+		cs.length > 0 ? "" : "empty",
+		...(f === ws ? ["focus"] : []),
+	])
+
+	return (
+		<label
+			css_classes={classes}
+			label={name}
+			width_request={config.corner}
+			height_request={config.corner}
+		>
+			<Gtk.GestureClick
+				button={Gdk.BUTTON_PRIMARY}
+				onPressed={() => {
+					hyprland.message(`dispatch workspace ${ws.id}`)
+				}}
+			/>
+		</label>
+	)
+
+}
+
+export function Workspaces ( {length: length = config.default_workspaces.length} : {length: number} )
+{
+	const fixed = workspaces(() =>
+		Array.from(
+			{ length },
+			(_, i) => hyprland.get_workspace(i + 1) || Hyprland.Workspace.dummy(i + 1, null),
+		),
+	)
+
+	return <box
+		class={"workspaces"}
+	>
+		<For each={length <= 0 ? workspaces : fixed}>
+      		{(ws) => <Workspace ws={ws} />}
+    	</For>
+	</box>;
 }

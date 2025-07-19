8d96fa48704103b66f59066a92cb1162cdffd828
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-07-19 19:03:08 -0300

    feat(ags.title): add scrolled bar around

--- configuração/local/ags/widget/bar/title/index.tsx
+++ configuração/local/ags/widget/bar/title/index.tsx
@@ -1,13 +1,13 @@
-import { createBinding, With } from "ags"
-import { execAsync } from "ags/process"
 import { Astal, Gtk, Gdk } from "ags/gtk4"
 import { config } from "../../../app"
 import { Label } from "./label"
 
 export function Title() {
-	return <button
-		onClicked={() => execAsync("echo hello").then(console.log)}
+	return <scrolledwindow
+		propagate_natural_width
+		vscrollbar_policy={Gtk.PolicyType.NEVER}
+		maxContentWidth={config.title_max_width}
 	>
 		<Label />
-	</button>
+	</scrolledwindow>
 }

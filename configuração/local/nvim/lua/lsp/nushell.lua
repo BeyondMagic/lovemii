7e2c3f552ffe3534ce6870d275c93d81d9d97cd9
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-08-08 23:43:34 -0300

    style(nu.lua): break lines in cmd definition

--- configuração/local/nvim/lua/lsp/nushell.lua
+++ configuração/local/nvim/lua/lsp/nushell.lua
@@ -1,5 +1,12 @@
-require'lspconfig'.nushell.setup {
-	cmd = { "nu", "--include-path", "/home/dream/.config/nushell/scripts", "--include-path", "/home/dream/.local/share/nupm/modules/", "--lsp"},
+require 'lspconfig'.nushell.setup {
+	cmd = {
+		"nu",
+		"--include-path",
+		"/home/dream/.config/nushell/scripts",
+		"--include-path",
+		"/home/dream/.local/share/nupm/modules/",
+		"--lsp"
+	},
 
 	-- Default.
 	capabilities = CAPABILITIES,

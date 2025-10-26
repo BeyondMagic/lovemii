3e0029ebb1af94dad78e51299122bc4b0e4b1030
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-10-26 12:57:40 -0300

    refactor(ags.workspaces): just use gtk button

--- configuração/local/ags/widget/bar/workspaces/workspace.tsx
+++ configuração/local/ags/widget/bar/workspaces/workspace.tsx
@@ -1,8 +1,8 @@
-import { Astal, Gtk, Gdk } from "ags/gtk4"
+import { Gtk } from "ags/gtk4"
 import { hyprland } from "../../../services/hyprland";
-import { createBinding, createComputed, For } from "ags"
+import { createBinding, createComputed } from "ags"
 import { config } from "../../../app";
-import Hyprland from "gi://AstalHyprland";
+import type Hyprland from "gi://AstalHyprland";
 
 const focused = createBinding(hyprland(), "focused_workspace")
 
@@ -11,26 +11,25 @@ export function Workspace ({ ws } : { ws: Hyprland.Workspace })
 	const clients = createBinding(ws, "clients")
 	const name = createBinding(ws, "name").as((n) => n ?? `${ws.id}`)
 
-	const classes = createComputed([clients, focused], (cs, f) => [
-		"workspace",
-		cs.length > 0 ? "" : "empty",
-		...(f === ws ? ["focus"] : []),
-	])
+	const classes = createComputed([clients, focused], (cs, f) => {
+		const result = ["workspace"]
+		if (cs.length === 0)
+			result.push("empty")
+		if (f === ws)
+			result.push("focus")
+		return result
+	})
 
 	return (
-		<label
+		<Gtk.Button
 			css_classes={classes}
 			label={name}
 			width_request={config.corner}
 			height_request={config.corner}
-		>
-			   <Gtk.GestureClick
-				   button={Gdk.BUTTON_PRIMARY}
-				   onPressed={() => {
-					   hyprland().message(`dispatch workspace ${ws.id}`)
-				   }}
-			   />
-		</label>
+			onClicked={() => {
+				hyprland().message(`dispatch workspace ${ws.id}`)
+			}}
+		/>
 	)
 
 }
